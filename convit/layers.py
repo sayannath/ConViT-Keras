2e914a4c90bc0987df023c047f27bef6c84a6812
soumik12345 <19soumik.rakshit96@gmail.com>
2021-11-06 16:01:08 +0530

    Added layers and blocks

---
+++ convit/layers.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+
+class Patches(layers.Layer):
+    """Reference: https://keras.io/examples/vision/image_classification_with_vision_transformer/#implement-patch-creation-as-a-layer"""
+
+    def __init__(self, patch_size):
+        super(Patches, self).__init__()
+        self.patch_size = patch_size
+
+    def call(self, images):
+        batch_size = tf.shape(images)[0]
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size, self.patch_size, 1],
+            strides=[1, self.patch_size, self.patch_size, 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+        patch_dims = patches.shape[-1]
+        patches = tf.reshape(patches, [batch_size, -1, patch_dims])
+        return patches
+
+
+class PatchEncoder(layers.Layer):
+    """Reference: https://keras.io/examples/vision/image_classification_with_vision_transformer/#implement-the-patch-encoding-layer"""
+
+    def __init__(self, num_patches, projection_dim):
+        super(PatchEncoder, self).__init__()
+        self.num_patches = num_patches
+        self.projection = layers.Dense(units=projection_dim)
+        self.position_embedding = layers.Embedding(
+            input_dim=num_patches, output_dim=projection_dim
+        )
+
+    def call(self, patch):
+        positions = tf.range(start=0, limit=self.num_patches, delta=1)
+        encoded = self.projection(patch) + self.position_embedding(positions)
+        return encoded
